293027875892a8342cf3f16f0a5a506c8956e954
Frizzus <loiccanin.pro@gmail.com>
2023-12-01 11:54:20 +0100

    extraction des données app1 d'un jpg sous forme de tableau d'hexadecimal

--- exif.py
+++ exif.py
@@ -9,7 +9,20 @@ args = parser.parse_args()
 img_file = open(args.file_path, "rb")
 img_bytes = img_file.read()
 
-for i in range(len(img_bytes)-1):
-    # print(hex(img_bytes[i]))
-    if hex(img_bytes[i]) == "0xff" and hex(img_bytes[i+1]) == "0xe1":
-        print("l'index du marqueur APP1 se trouve à l'index : ", i)
+in_app1 = False
+app1_hexs:list[str] = []
+i = 0
+while i < len(img_bytes):
+    if in_app1:
+        app1_hexs.append(hex(img_bytes[i]))
+        if hex(img_bytes[i]) == "0xff":
+            in_app1 = False
+            app1_hexs = app1_hexs[:-1]
+            print(app1_hexs)
+            print("début du marker suivant")
+        i += 1
+    else:
+        if hex(img_bytes[i]) == "0xff" and hex(img_bytes[i+1]) == "0xe1":
+            print("l'index du marqueur APP1 se trouve à l'index : ", i)
+            in_app1 = True
+        i += 2
